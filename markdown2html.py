e3b5989fdbe92f7074e7149438485552eb3ecca5
truuue <noahvernhet@gmail.com>
2024-04-19 00:31:46 +0200

    Refactor pycodestyle markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -10,8 +10,10 @@ First argument is the name of the Markdown file
 Second argument is the output file name
 Requirements:
 
-If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
-If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+If the number of arguments is less than 2:
+print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
+If the Markdown file doesn’t exist:
+print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0
 """
 
